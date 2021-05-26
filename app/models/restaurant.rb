66ebaad919a10c24bf9afe315df3290f38acdb1a
Marcus Allen <emkay1993@gmail.com>
2021-05-26 15:22:49 +0100

    Add routing and models

---
+++ app/models/restaurant.rb
@@ -0,0 +1,9 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 50 }
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  validates :category, presence: true
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def create_category
+  arr = %w[chinese italian japanese french belgian]
+  arr.sample
+end
+
+def create_array
+  arr = []
+  5.times do
+    arr << { name: Faker::Restaurant.name, address: Faker::Address.full_address,
+             phone_number: Faker::PhoneNumber.cell_phone, category: create_category }
+  end
+  arr
+end
+
+puts 'Cleaning Database'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants = create_array
+
+restaurants.each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  review = Review.new({ content: Faker::TvShows::MichaelScott.quote, rating: rand(0..5) })
+  review.restaurant = restaurant
+  review.save!
+  puts "Created #{restaurant.name}"
+end
+
+puts 'Finished!'
